e63d94a793fd77eb7bc559491ff565498e3e357b
ductai98 <ronin32014@gmail.com>
2019-05-21 16:54:27 +0700

    CRUD Loai san pham

--- admin-page/app.js
+++ admin-page/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/productsRouter');
 var orderRouter = require('./routes/orderRouter');
+var categoryRouter = require('./routes/categoryRouter');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/order', orderRouter);
+app.use('/category', categoryRouter);
 
 
 
