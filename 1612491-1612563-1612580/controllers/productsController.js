49a89bc647ae0f112f21d762395cac2fd724b339
Phat Ho <hoangphatho@gmail.com>
2019-06-12 14:14:06 +0700

    Update dữ liệu cho các sản phẩm

--- 1612491-1612563-1612580/controllers/productsController.js
+++ 1612491-1612563-1612580/controllers/productsController.js
@@ -62,8 +62,8 @@ exports.index2 = function (req,res,next) {
     });
 };
 
-exports.index1 = function (req,res, next) {
-    res.render('products/index1',
+exports.dienthoai = function (req,res, next) {
+    res.render('products/dienthoai',
     {
         PhoneProduct: [{
             image: '/images/m1.jpg',
@@ -124,4 +124,466 @@ exports.index1 = function (req,res, next) {
         }],
         title: 'Đồ điện tử'
     });
+};
+
+exports.phukien = function(req, res, next){
+  res.render('products/phukien',
+  {
+      Accessories1: [{
+          image: '/images/cap1.jpg',
+          name: 'Cáp sạc micro USB',
+          sales: '40.000đ',
+          price:'80.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/cap2.jpg',
+          name: 'Cáp Type-C',
+          sales: '55.000đ',
+          price:'100.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/tainghe1.jpg',
+          name: 'Tai nghe Kanen IP-218',
+          sales: '90.000đ',
+          price: '150.000đ',
+          uri:'/single'
+      }],
+      Accessories2: [{
+          image: '/images/cap3.jpg',
+          name: 'Cáp lightning iWalk',
+          sales: '175.000đ',
+          price:'250.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/cap4.jpg',
+          name: 'Cáp lightning eValu dài 2m',
+          sales: '90.000đ',
+          price:'150.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/tainghe3.jpg',
+          name: 'Tai nghe Awei Q19Hi',
+          sales: '105.000đ',
+          price: '150.000đ',
+          uri:'/single'
+      }],
+      Accessories3: [{
+          image: '/images/thenho1.jpg',
+          name: 'Thẻ nhớ MicroSD 32GB',
+          sales: '210.000đ',
+          price:'300.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/thenho2.jpg',
+          name: 'Thẻ nhớ MicroSD 16GB',
+          sales: '165.000đ',
+          price:'220.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/thenho3.jpg',
+          name: 'Thẻ nhớ MicroSD 8GB',
+          sales: '105.000đ',
+          price: '150.000đ',
+          uri:'/single'
+      }],
+      title: 'Phụ kiện điện thoại'});
+};
+
+exports.cases = function(req, res, next){
+  res.render('products/cases',
+  {
+      Op1: [{
+          image: '/images/oplung1.jpg',
+          name: 'Ốp lưng Oppo F9 nhựa dẻo',
+          sales: '16.000đ',
+          price:'20.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/oplung2.jpg',
+          name: 'Ốp lưng iPhone 6/6s nhựa dẻo',
+          sales: '16.000đ',
+          price:'20.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/oplung3.jpg',
+          name: 'Ốp lưng iPhone XS Max nhựa dẻo',
+          sales: '16.000đ',
+          price: '20.000đ',
+          uri:'/single'
+      }],
+      Op2: [{
+          image: '/images/oplung4.jpg',
+          name: 'Ốp lưng iPhone 6/6s nhựa cứng',
+          sales: '16.000đ',
+          price:'20.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/oplung5.jpg',
+          name: 'Ốp lưng Galaxy S9 nhựa dẻo',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/oplung6.jpg',
+          name: 'Ốp lưng iPad 9.7inch nắp gập',
+          sales: '125.000đ',
+          price: '250.000đ',
+          uri:'/single'
+      }],
+      Op3: [{
+          image: '/images/oplung7.jpg',
+          name: 'Ốp lưng Tab A6 7inch nắp gập',
+          sales: '125.000đ',
+          price:'250.000đ',
+          uri:'/single'
+      }, {
+          image: '/images/oplung8.jpg',
+          name: 'Ốp lưng Tab A6 7inch nắp gập nhiều màu',
+          sales: '140.000đ',
+          price:'280.000đ',
+          uri:'/single',
+
+      }, {
+          image: '/images/oplung9.jpg',
+          name: 'Ốp lưng iPad 9.7inch màu GOLD',
+          sales: '150.000đ',
+          price: '300.000đ',
+          uri:'/single'
+      }],
+      title: 'Ốp lưng'});
+};
+
+exports.screenprotector = function(req, res, next){
+  res.render('products/screenprotector',
+  {
+      Screen1: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/86145/mieng-dan-lung-iphone-7-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán iPhone 7Plus/8Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/193418/mieng-dan-man-hinh-iphone-xs-max-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone XS Max',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/68268/mieng-dan-film-trong-dien-thoai-3m-avatar-1-600x600.jpg',
+          name: 'Miếng dán mặt lưng 3M cho điện thoại 5inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen2: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/202055/mieng-dan-man-hinh-oppo-f11-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Oppo F11',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/99080/mieng-dan-man-hinh-galaxy-s8-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Galaxy S8 Plus/ S9 Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69533/mieng-dan-film-trong-dien-thoai-6-inch-avatar-1-600x600.jpg',
+          name: 'Miếng dán film điện thoại 6inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen3: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69914/mieng-dan-man-hinh-iphone-6-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone 6',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/187278/mieng-dan-man-hinh-galaxy-note-9-avatar-600x600.jpg',
+          name: 'Miến dán màn hình Galaxy Note 9',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/191768/mieng-dan-man-hinh-nokia-61-plus-gos-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình Nokia 61 Plus',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      title: 'Miếng dán màn hình'});
+};
+
+exports.powerbank = function(req, res, next){
+  res.render('products/screenprotector',
+  {
+      Screen1: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/86145/mieng-dan-lung-iphone-7-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán iPhone 7Plus/8Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/193418/mieng-dan-man-hinh-iphone-xs-max-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone XS Max',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/68268/mieng-dan-film-trong-dien-thoai-3m-avatar-1-600x600.jpg',
+          name: 'Miếng dán mặt lưng 3M cho điện thoại 5inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen2: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/202055/mieng-dan-man-hinh-oppo-f11-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Oppo F11',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/99080/mieng-dan-man-hinh-galaxy-s8-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Galaxy S8 Plus/ S9 Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69533/mieng-dan-film-trong-dien-thoai-6-inch-avatar-1-600x600.jpg',
+          name: 'Miếng dán film điện thoại 6inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen3: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69914/mieng-dan-man-hinh-iphone-6-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone 6',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/187278/mieng-dan-man-hinh-galaxy-note-9-avatar-600x600.jpg',
+          name: 'Miến dán màn hình Galaxy Note 9',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/191768/mieng-dan-man-hinh-nokia-61-plus-gos-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình Nokia 61 Plus',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      title: 'Miếng dán màn hình'});
+};
+
+exports.tablets = function(req, res, next){
+  res.render('products/screenprotector',
+  {
+      Screen1: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/86145/mieng-dan-lung-iphone-7-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán iPhone 7Plus/8Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/193418/mieng-dan-man-hinh-iphone-xs-max-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone XS Max',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/68268/mieng-dan-film-trong-dien-thoai-3m-avatar-1-600x600.jpg',
+          name: 'Miếng dán mặt lưng 3M cho điện thoại 5inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen2: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/202055/mieng-dan-man-hinh-oppo-f11-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Oppo F11',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/99080/mieng-dan-man-hinh-galaxy-s8-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Galaxy S8 Plus/ S9 Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69533/mieng-dan-film-trong-dien-thoai-6-inch-avatar-1-600x600.jpg',
+          name: 'Miếng dán film điện thoại 6inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen3: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69914/mieng-dan-man-hinh-iphone-6-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone 6',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/187278/mieng-dan-man-hinh-galaxy-note-9-avatar-600x600.jpg',
+          name: 'Miến dán màn hình Galaxy Note 9',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/191768/mieng-dan-man-hinh-nokia-61-plus-gos-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình Nokia 61 Plus',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      title: 'Miếng dán màn hình'});
+};
+
+exports.watch = function(req, res, next){
+  res.render('products/screenprotector',
+  {
+      Screen1: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/86145/mieng-dan-lung-iphone-7-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán iPhone 7Plus/8Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/193418/mieng-dan-man-hinh-iphone-xs-max-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone XS Max',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/68268/mieng-dan-film-trong-dien-thoai-3m-avatar-1-600x600.jpg',
+          name: 'Miếng dán mặt lưng 3M cho điện thoại 5inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen2: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/202055/mieng-dan-man-hinh-oppo-f11-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Oppo F11',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/99080/mieng-dan-man-hinh-galaxy-s8-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Galaxy S8 Plus/ S9 Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69533/mieng-dan-film-trong-dien-thoai-6-inch-avatar-1-600x600.jpg',
+          name: 'Miếng dán film điện thoại 6inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen3: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69914/mieng-dan-man-hinh-iphone-6-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone 6',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/187278/mieng-dan-man-hinh-galaxy-note-9-avatar-600x600.jpg',
+          name: 'Miến dán màn hình Galaxy Note 9',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/191768/mieng-dan-man-hinh-nokia-61-plus-gos-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình Nokia 61 Plus',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      title: 'Miếng dán màn hình'});
+};
+
+exports.smarthome = function(req, res, next){
+  res.render('products/screenprotector',
+  {
+      Screen1: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/86145/mieng-dan-lung-iphone-7-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán iPhone 7Plus/8Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/193418/mieng-dan-man-hinh-iphone-xs-max-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone XS Max',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/68268/mieng-dan-film-trong-dien-thoai-3m-avatar-1-600x600.jpg',
+          name: 'Miếng dán mặt lưng 3M cho điện thoại 5inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen2: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/202055/mieng-dan-man-hinh-oppo-f11-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Oppo F11',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/99080/mieng-dan-man-hinh-galaxy-s8-plus-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình Galaxy S8 Plus/ S9 Plus',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69533/mieng-dan-film-trong-dien-thoai-6-inch-avatar-1-600x600.jpg',
+          name: 'Miếng dán film điện thoại 6inch',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      Screen3: [{
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/69914/mieng-dan-man-hinh-iphone-6-avatar-1-600x600.jpg',
+          name: 'Miếng dán màn hình iPhone 6',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single'
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/187278/mieng-dan-man-hinh-galaxy-note-9-avatar-600x600.jpg',
+          name: 'Miến dán màn hình Galaxy Note 9',
+          sales: '40.000đ',
+          price:'50.000đ',
+          uri:'/single',
+
+      }, {
+          image: 'https://cdn.tgdd.vn/Products/Images/1363/191768/mieng-dan-man-hinh-nokia-61-plus-gos-avatar-600x600.jpg',
+          name: 'Miếng dán màn hình Nokia 61 Plus',
+          sales: '40.000đ',
+          price: '50.000đ',
+          uri:'/single'
+      }],
+      title: 'Miếng dán màn hình'});
 };
--- 1612491-1612563-1612580/routes/products.js
+++ 1612491-1612563-1612580/routes/products.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 var productsController = require('../controllers/productsController');
 
-router.get('/product1', productsController.index1);
+router.get('/dien-thoai', productsController.dienthoai);
+router.get('/phu-kien-dien-thoai', productsController.phukien);
+router.get('/cases', productsController.cases);
+router.get('/screen-protector', productsController.screenprotector);
+router.get('/power-bank', productsController.powerbank);
+router.get('/tablets', productsController.tablets);
+router.get('/watch', productsController.watch);
+router.get('/smart-home', productsController.smarthome);
 router.get('/product2', productsController.index2);
 
 module.exports = router;
